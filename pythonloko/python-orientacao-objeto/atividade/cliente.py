1b6aea407c8bc9154caaaa645de665e7918b6511
KITTz1n <wellcomepudim@gmail.com>
2024-08-08 20:02:57 -0300

    att

--- pythonloko/python-orientacao-objeto/atividade/cliente.py
+++ pythonloko/python-orientacao-objeto/atividade/cliente.py
@@ -1,7 +1,7 @@
-class Cliente:
+class Cliente:#cria uma classe chamada Cliente
 
-    def __init__(self, nome, cpf, idade):
-        self.nome = nome
-        self.cpf = cpf
-        self.idade = idade
-        print(f'\nMuito obrigado Sr.{nome} seu cliente foi\nregistrado com sucesso\n')
+    def __init__(self, nome, cpf, idade):#funcao init para quando chamar a classe Conta pedir variaveis: nome do cliente, cpf do cliente, idade do cliente
+        self.nome = nome#armazena na classe o nome do cliente
+        self.cpf = cpf#armazena na classe o cpf do cliente
+        self.idade = idade#armazena na classe a idade do cliente
+        print(f'\nMuito obrigado Sr.{nome} seu cliente foi\nregistrado com sucesso\n')#mostra que o cliente foi registrado com sucesso
--- pythonloko/python-orientacao-objeto/atividade/conta.py
+++ pythonloko/python-orientacao-objeto/atividade/conta.py
@@ -1,23 +1,23 @@
-class Conta():
+class Conta():#cria uma classe chamada Conta
 
-    def __init__(self,numero_conta,cpf_conta,saldo,limite,nome):
-        self.numero_conta = numero_conta
-        self.cpf_conta = cpf_conta
-        self.saldo = saldo
-        self.limite = limite
-        self.nome = nome
-        print(f'\nConta criada com sucesso,Sr.{nome}\n')
+    def __init__(self,numero_conta,cpf_conta,saldo,limite,nome):#funcao init para quando chamar a classe Conta pedir variaveis: num,cpf,saldo,limite da conta e o nome do cliente
+        self.numero_conta = numero_conta#armazena na classe o numero da conta
+        self.cpf_conta = cpf_conta#armazena na classe o cpf da conta
+        self.saldo = saldo#armazena na classe o saldo da conta
+        self.limite = limite#armazena na classe o limite da conta
+        self.nome = nome#armazena na classe o nome do cliente que criou a conta
+        print(f'\nConta criada com sucesso,Sr.{nome}\n')#mostra que a conta foi criada com sucesso
 
-    def Sacar(self,v):
-        self.saldo -= v
-        if self.saldo < 0:
-            devendo = self.saldo * -1
-            print(f'\nVoce esta devendo: R${devendo}\n')
+    def Sacar(self,v):#funcao de sacar para quando dentro da classe chama-la e ela pede um valor que nesse caso e o valor de V
+        self.saldo -= v#faz subtrai V(valor inserido na funcao) de saldo
+        if self.saldo < 0:#se o saldo for menor que zero
+            devendo = self.saldo * -1#calcula o numero que vc deve
+            print(f'\nVoce esta devendo: R${devendo}\n')#mostra o quando vc esta devendo atualmete
 
-    def Depositar(self,v):
-        if v + self.saldo > self.limite:
-            resp = v + self.saldo - self.limite
-            print(f'\nVoce excedeu R${resp} o seu limite e de R${self.limite}\nFoi devolvido ao Sr.{self.nome} R${resp}')
-            self.saldo = self.saldo + v - resp
-        else:
-            self.saldo += v
+    def Depositar(self,v):#funcao de depositar para quando dentro da classe chama-la e ela pede um valor que nesse caso e o valor de V
+        if v + self.saldo > self.limite:#se a soma de V(valor inserido na funcao) com o saldo atual da conta for maior que o limite da conta
+            resp = v + self.saldo - self.limite#calcular o excesso de deposito
+            print(f'\nVoce excedeu R${resp} o seu limite e de R${self.limite}\nFoi devolvido ao Sr.{self.nome} R${resp}')#mostra a quantidade excedida, mostra o limite e a quantidade devolvida
+            self.saldo = self.saldo + v - resp#calcula a soma do saldo com o V(valor inserido na funcao) menos o valor de excesso do deposito
+        else:#se nao exceder
+            self.saldo += v#somar o saldo com o V(valor inserido na funcao)
